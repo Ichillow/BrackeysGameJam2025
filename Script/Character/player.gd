c2834b7ed3cc71af71c902e25eeb02a7743df461
Loïs Maytraud <lois.maytraud@gmail.com>
2025-08-29 16:45:02 +0200

    Implements player movement and animations
    Adds player movement based on clicking stands in the scene.
    Introduces "run" animation and manages animation states for idle and run.
    Configures canvas items stretch mode for consistent display.

--- Script/Character/player.gd
+++ Script/Character/player.gd
@@ -6,18 +6,54 @@ extends CharacterBody3D
 @onready var animation_player: AnimationPlayer = $MeshInstance3D/AnimationPlayer
 
 var state =""
+var target_position: Vector3
+var moving: bool = false
+
 
 func _ready():
 	state = "Idle"
 	animation_state()
 
+#Gestion du clique sur les stands pour déplacer le joueur
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		var camera = get_viewport().get_camera_3d()
+		if camera:
+			var from = camera.project_ray_origin(event.position)
+			var to = from + camera.project_ray_normal(event.position) * 1000.0
+			
+			var space_state = get_world_3d().direct_space_state
+			var query = PhysicsRayQueryParameters3D.create(from, to)
+			var result = space_state.intersect_ray(query)
+			
+			if result:
+				var obj = result.collider
+				if obj.is_in_group("stands"):
+					target_position = obj.global_transform.origin
+					moving = true
+					state = "Run"
+					animation_state()
+					obj.clicked()
+
+
+
+#Gestion de la physique du joueur
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	move_and_slide()
 
-func _process(_delta):
-	pass
+	if moving:
+		var direction = (target_position - global_transform.origin)
+		if direction.length() > 0.1:
+			direction = direction.normalized()
+			velocity = direction * speed
+			move_and_slide()
+		else:
+			velocity = Vector3.ZERO
+			moving = false
+
+
 
 
 func animation_state():
--- Script/Screen.gd
+++ Script/Screen.gd
@@ -6,25 +6,3 @@ extends Node3D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		var from = camera.project_ray_origin(event.position)
-		var to = from + camera.project_ray_normal(event.position) * 1000.0
-
-		var space_state = get_world_3d().direct_space_state
-		var ray_params = PhysicsRayQueryParameters3D.new()
-		ray_params.from = from
-		ray_params.to = to
-		var result = space_state.intersect_ray(ray_params)
-
-		if result:
-			var obj = result.collider
-			# Levier toggle : clic = on/off
-			if obj.is_in_group("stands"):
-				if event.pressed:
-					obj.clicked()
